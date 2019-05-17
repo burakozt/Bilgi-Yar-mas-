a21318dd32cec35489ce627bfdc3c84400f2e76a
burakozt <49121740+burakozt@users.noreply.github.com>
2019-05-18 01:15:44 +0300

    Add files via upload

---
+++ Yarişma/Yarişma/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Yarişma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(296, 151);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 142);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 26;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(296, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(307, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Yanlış";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Doğru";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(307, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Level";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(157, 243);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(62, 49);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(189, 116);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 344);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Yarişma/Yarişma/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Yarişma
+{
+    public partial class Form1 : Form
+    {
+        int level = 1, Doğru = 0, yanlış = 0;
+        string[] sorular = { "2.Dünya Savaşı Nerenin İşgali İle Başlamıştır?", "Türkiyenin İlk Başbakanı Kimdir?", "Kiev Hangi Ülkenin Bir Şehridir?", "Aşağıdaki Ülkelerden Hangisi Amerika Kıtasında Değildir?" };
+        int k = 0, l = 0, t = 0, j = 0;
+        Random salla = new Random();
+        string tmp;
+
+        public Form1()
+        {
+         
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            k = salla.Next(j, 4);
+            richTextBox1.Text = sorular[k];
+            switch (k)
+            {
+                case 0: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Fransa";
+                    button4.Text = "D=Sovyetler Birliği";
+
+                    break;
+                case 1: button1.Text = "A=Rauf Orbay";
+                    button2.Text = "B=Mustafa Kemal Atatürk";
+                    button3.Text = "C=İsmet İnönü";
+                    button4.Text = "D=Ali Fethi Okyar";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 2: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Ukrayna";
+                    button4.Text = "D=Rusya";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 3: button1.Text = "A=Peru";
+                    button2.Text = "B=Portekiz";
+                    button3.Text = "C=Şili";
+                    button4.Text = "D=Panama";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                    
+
+            }
+                     j++;
+        
+        
+        
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            k = salla.Next(j, 4);
+            richTextBox1.Text = sorular[k];
+            switch (k)
+            {
+                case 0: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Fransa";
+                    button4.Text = "D=Sovyetler Birliği";
+
+                    break;
+                case 1: button1.Text = "A=Rauf Orbay";
+                    button2.Text = "B=Mustafa Kemal Atatürk";
+                    button3.Text = "C=İsmet İnönü";
+                    button4.Text = "D=Ali Fethi Okyar";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 2: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Ukrayna";
+                    button4.Text = "D=Rusya";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 3: button1.Text = "A=Peru";
+                    button2.Text = "B=Portekiz";
+                    button3.Text = "C=Şili";
+                    button4.Text = "D=Panama";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+
+
+
+            }
+            j++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            k = salla.Next(j, 4);
+            richTextBox1.Text = sorular[k];
+            switch (k)
+            {
+                case 0: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Fransa";
+                    button4.Text = "D=Sovyetler Birliği";
+
+                    break;
+                case 1: button1.Text = "A=Rauf Orbay";
+                    button2.Text = "B=Mustafa Kemal Atatürk";
+                    button3.Text = "C=İsmet İnönü";
+                    button4.Text = "D=Ali Fethi Okyar";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 2: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Ukrayna";
+                    button4.Text = "D=Rusya";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 3: button1.Text = "A=Peru";
+                    button2.Text = "B=Portekiz";
+                    button3.Text = "C=Şili";
+                    button4.Text = "D=Panama";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+
+
+
+            }
+            j++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            k = salla.Next(j, 4);
+            richTextBox1.Text = sorular[k];
+            switch (k)
+            {
+                case 0: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Fransa";
+                    button4.Text = "D=Sovyetler Birliği";
+
+                    break;
+                case 1: button1.Text = "A=Rauf Orbay";
+                    button2.Text = "B=Mustafa Kemal Atatürk";
+                    button3.Text = "C=İsmet İnönü";
+                    button4.Text = "D=Ali Fethi Okyar";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 2: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Ukrayna";
+                    button4.Text = "D=Rusya";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 3: button1.Text = "A=Peru";
+                    button2.Text = "B=Portekiz";
+                    button3.Text = "C=Şili";
+                    button4.Text = "D=Panama";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+
+
+
+            }
+            j++;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            k = salla.Next(j, 4);
+            richTextBox1.Text = sorular[k];
+            switch (k)
+            {
+                case 0: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Fransa";
+                    button4.Text = "D=Sovyetler Birliği";
+
+                    break;
+                case 1: button1.Text = "A=Rauf Orbay";
+                    button2.Text = "B=Mustafa Kemal Atatürk";
+                    button3.Text = "C=İsmet İnönü";
+                    button4.Text = "D=Ali Fethi Okyar";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 2: button1.Text = "A=Polonya";
+                    button2.Text = "B=Almanya";
+                    button3.Text = "C=Ukrayna";
+                    button4.Text = "D=Rusya";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+                case 3: button1.Text = "A=Peru";
+                    button2.Text = "B=Portekiz";
+                    button3.Text = "C=Şili";
+                    button4.Text = "D=Panama";
+                    tmp = sorular[k];
+                    sorular[k] = sorular[j];
+                    sorular[j] = tmp;
+                    break;
+
+
+
+            }
+            j++;
+        }
+    }
+}
